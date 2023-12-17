4e9a49d11d2aabc9c273521e61d7c3ed833feb44
Tyler <105254530+tylerpierson@users.noreply.github.com>
2023-12-17 12:23:50 -0600

    Magic 8 Ball express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -13,6 +13,15 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`<h1>${req.params.total * (req.params.tipPercentage * .01)}</h1>`)
 })
 
+const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:question', (req, res) => {
+    res.send(`
+    <h2>${req.params.question}</h2>
+    <h1>${responses[Math.floor(Math.random() * responses.length)]}</h1>
+    `)
+})
+
 app.listen(3000, () => {
     console.log('Port is now ACTIVE')
 })
