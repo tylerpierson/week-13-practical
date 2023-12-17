063270fd3d82e2d7b008c1c93e205c8e9920389b
Tyler <105254530+tylerpierson@users.noreply.github.com>
2023-12-17 12:14:11 -0600

    Tip Calculator express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -9,6 +9,10 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`<h1>Howdy, ${req.params.name}!</h1>`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`<h1>${req.params.total * (req.params.tipPercentage * .01)}</h1>`)
+})
+
 app.listen(3000, () => {
     console.log('Port is now ACTIVE')
 })
